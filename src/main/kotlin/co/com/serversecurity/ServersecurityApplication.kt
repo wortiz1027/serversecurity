3fcbbb12ccb87851eb61670fc308633e5f733f71
Wilman Ortiz <willman.ortiz@outlook.com>
2018-05-03 20:54:14 -0500

    feat: Servidor Oauth2 + JWT
    Creación de servidor de autenticacion con spring boot
    spring security kotlin y jwt

---
+++ src/main/kotlin/co/com/serversecurity/ServersecurityApplication.kt
@@ -0,0 +1,11 @@
+package co.com.serversecurity
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServersecurityApplication
+
+fun main(args: Array<String>) {
+    runApplication<ServersecurityApplication>(*args)
+}
